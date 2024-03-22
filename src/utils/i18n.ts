311d302d734e77047c8dfc9aa9d90a963d6a9a7a
yuwol <yuwol036@gmail.com>
2024-03-22 23:56:01 +0900

    :truck: Move i18n resources to `locales` directory

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,15 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import usTranslations from "@/translations/en_US.json";
-import krTranslations from "@/translations/ko_KR.json";
+
+import usTranslations from "@/locales/en_US.json";
+import krTranslations from "@/locales/ko_KR.json";
 
 const resources = {
-  en_US: {
-    translation: usTranslations,
-  },
-  ko_KR: {
-    translation: krTranslations,
-  },
+  en_US: { translation: usTranslations },
+  ko_KR: { translation: krTranslations },
 };
 
 i18n.use(initReactI18next).init({
