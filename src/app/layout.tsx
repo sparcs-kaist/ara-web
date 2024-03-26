1ba06cb746e519228c00389d5dff5fac2acf6250
yuwol <yuwol036@gmail.com>
2024-03-26 20:05:39 +0900

    :lipstick: Add pretendard font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import Providers from "@/app/providers";
+import { pretendard } from "@/styles/fonts";
 
 export const metadata: Metadata = {
   title: "Ara",
@@ -8,7 +9,7 @@ export const metadata: Metadata = {
 };
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <html lang="ko">
+  <html lang="ko" className={pretendard.className}>
     <body>
       <Providers>{children}</Providers>
     </body>
