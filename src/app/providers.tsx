9d18112c928a6fc97dc942715063e69453f2cd0e
yuwol <yuwol036@gmail.com>
2024-04-28 17:58:14 +0900

    :heavy_plus_sign: Add tanstack query dev dependencies

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,30 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { I18nextProvider } from "react-i18next";
 
 import i18n from "@/utils/i18n";
 
-const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
-);
+const makeQueryClient = () => new QueryClient();
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+const getQueryClient = () => {
+  if (typeof window === "undefined") return makeQueryClient();
+  if (!browserQueryClient) browserQueryClient = makeQueryClient();
+  return browserQueryClient;
+};
+
+const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
 
 export default Providers;
