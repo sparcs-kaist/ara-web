d474870116e2a9a125a72999c4862134fccb70d9
yuwol <yuwol036@gmail.com>
2024-04-28 17:59:32 +0900

    :label: Add board types

---
+++ src/constants/enum.ts
@@ -0,0 +1,10 @@
+// FIXME: Use better way to define enum
+export const enum NameType {
+  NICKNAME = 1,
+  ANONYMOUS = 2,
+  NICKNAME_ANONYMOUS = 3,
+  REALNAME = 4,
+  NICKNAME_REALNAME = 5,
+  ANONYMOUS_REALNAME = 6,
+  NICKNAME_ANONYMOUS_REALNAME = 7,
+}
