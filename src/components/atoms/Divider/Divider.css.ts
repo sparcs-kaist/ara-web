b5c9594a11691f77c08aa4483b3f09baf3603835
Yumin Cho <dbals081216@gmail.com>
2024-04-29 23:00:26 +0900

    :sparkles: Impl Divider

---
+++ src/components/atoms/Divider/Divider.css.ts
@@ -0,0 +1,15 @@
+import { sprinkles } from "@/styles/sprinkles.css";
+
+export const divX = sprinkles({
+  border: "none",
+  width: "fill",
+  height: "min",
+  backgroundColor: "neutralLight2",
+});
+
+export const divY = sprinkles({
+  border: "none",
+  width: "min",
+  height: "fill",
+  backgroundColor: "neutralLight2",
+});
