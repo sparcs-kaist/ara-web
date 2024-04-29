d604e9f3a4ecbd569d2db76d1436e333d97bbc41
Yumin Cho <dbals081216@gmail.com>
2024-04-29 20:15:07 +0900

    :sparkles: Impl Dropdown

---
+++ src/components/atoms/Dropdown/Dropdown.css.ts
@@ -0,0 +1,52 @@
+import { style } from "@vanilla-extract/css";
+
+import { sprinkles } from "@/styles/sprinkles.css";
+import { vars } from "@/styles/theme.css";
+
+import Anchors from "../Anchor/Anchor.css";
+
+export const dropdown = style([]);
+
+export const dropdownButton = style([
+  Anchors.textAnchor,
+  sprinkles({
+    display: "flex",
+    alignItems: "center",
+  }),
+  {
+    background: "none",
+    border: "none",
+    cursor: "pointer",
+  },
+]);
+
+export const dropdownAnchorList = style([
+  sprinkles({
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "white",
+    padding: "xs",
+    borderRadius: "sm",
+    marginTop: "sm",
+  }),
+  {
+    position: "absolute",
+    width: "15rem",
+    boxShadow: `0rem 0.2rem 0.8rem 0rem ${vars.color.layerImage}`,
+  },
+]);
+
+export const dropdownAnchor = style([
+  sprinkles({
+    paddingX: "sm",
+    paddingY: "xs",
+    typeScale: "small",
+    backgroundColor: "white",
+  }),
+  {
+    flex: 1,
+    ":hover": {
+      backgroundColor: vars.color.neutralLight3,
+    },
+  },
+]);
---
+++ src/components/atoms/Dropdown/Dropdown.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { ChevronDown, ChevronUp } from "react-feather";
+import { useTranslation } from "react-i18next";
+
+import { BaseBoard } from "@/types";
+import i18n from "@/utils/i18n";
+
+import * as styles from "./Dropdown.css";
+
+interface DropdownProps {
+  title: string;
+  isOpen: boolean;
+  onToggle: () => void;
+  boards: BaseBoard[];
+}
+
+export const Dropdown: React.FC<DropdownProps> = ({ title, isOpen, onToggle, boards }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={styles.dropdown}>
+      <button className={styles.dropdownButton} onClick={onToggle}>
+        {t(title)}
+        {isOpen ? <ChevronUp size={16} /> : <ChevronDown size={16} />}
+      </button>
+      {isOpen && (
+        <ul className={styles.dropdownAnchorList}>
+          {boards.map((board) => (
+            <li key={board.id} className={styles.dropdownAnchor}>
+              {/* 클릭 가능 영역 조정 */}
+              <Link href="#">{i18n.language == "ko_KR" ? board.koName : board.enName}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
