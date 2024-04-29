4895c8562265baf8dbe8bae93cd3607cab6a3f75
Yumin Cho <dbals081216@gmail.com>
2024-04-29 23:17:41 +0900

    :sparkles: Impl Footer

---
+++ src/components/molecules/Footer/Footer.css.ts
@@ -0,0 +1,32 @@
+import { style } from "@vanilla-extract/css";
+
+import { sprinkles } from "@/styles/sprinkles.css";
+
+export const footer = style([
+  sprinkles({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "md",
+    color: "neutralLight1",
+  }),
+  {
+    paddingTop: "6.4rem",
+    paddingBottom: "3.2rem",
+  },
+]);
+
+export const linkList = sprinkles({
+  display: "flex",
+});
+
+export const link = sprinkles({
+  display: "inline-block",
+  paddingX: "sm",
+  textAlign: "center",
+});
+
+export const caution = sprinkles({
+  textAlign: "center",
+  textWrap: "balance",
+});
