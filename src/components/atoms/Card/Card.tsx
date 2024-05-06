234534f26d9645aabd786053186149df3e304eaa
Yejune Ko <koyejune0302@gmail.com>
2024-05-06 13:52:42 +0900

    :pencil2: fix typo

--- src/components/atoms/Card/Card.tsx
+++
@@ -1,5 +0,0 @@
-import * as styles from "./Card.css";
-
-export const Card: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div className={styles.card}>{children}</div>;
-};
