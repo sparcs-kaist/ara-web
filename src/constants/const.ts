6db38eb43373717c4e446088424399d11287d3a2
Yejune Ko <koyejune0302@gmail.com>
2024-05-20 23:42:53 +0900

    :truck: enum.ts to const.ts

---
+++ src/constants/const.ts
@@ -0,0 +1,16 @@
+// FIXME: Use better way to define enum
+export const NameType = {
+  NICKNAME: 1,
+  ANONYMOUS: 2,
+  NICKNAME_ANONYMOUS: 3,
+  REALNAME: 4,
+  NICKNAME_REALNAME: 5,
+  ANONYMOUS_REALNAME: 6,
+  NICKNAME_ANONYMOUS_REALNAME: 7,
+} as const;
+
+export const ResponseStatus = {
+  BEFORE_UPVOTES: "before_upvotes",
+  BEFORE_RESPONSE: "before_response",
+  AFTER_RESPONSE: "after_response",
+} as const;
