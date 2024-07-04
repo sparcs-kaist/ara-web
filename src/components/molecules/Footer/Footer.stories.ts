9c173534d9e605e4b51eb44f738c6f035369c8ae
yuwol <yuwol036@gmail.com>
2024-07-04 16:54:13 +0900

    :technologist: Add Footer story

---
+++ src/components/molecules/Footer/Footer.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Footer } from "./Footer";
+
+const meta = {
+  title: "Footer",
+  component: Footer,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
