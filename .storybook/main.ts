6f135dc921e7cbe005d62b9ba9140254aa00beae
yuwol <yuwol036@gmail.com>
2024-07-04 15:23:01 +0900

    :wrench: Configure storybook for vanilla-extract

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import { VanillaExtractPlugin } from "@vanilla-extract/webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -8,12 +10,34 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack",
   ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
   staticDirs: ["../public"],
+  webpackFinal: (config) => {
+    config.plugins?.push(new VanillaExtractPlugin(), new MiniCssExtractPlugin());
+    config.module?.rules?.forEach((rule) => {
+      if (rule && rule !== "..." && rule.test instanceof RegExp && rule.test.test("test.css")) {
+        rule.exclude = /\.vanilla\.css$/i;
+      }
+    });
+    config.module?.rules?.push({
+      test: /\.vanilla\.css$/i,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: require.resolve("css-loader"),
+          options: {
+            url: false,
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default config;
